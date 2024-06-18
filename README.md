247224f04528efc90df0d467eefdfd23c6c35ad6
Coriolan <andriamiharinjaracoriolanr.acr@gmail.com>
2024-06-18 19:54:02 +0300

    first use of JSON; détails are gotten from json file.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 Bibliotheka is the name I gave to my personal work at the web development exam.
+
+<!--
+    1. Créer une interface utilisateur avec HTML et CSS pour permettre à l'utilisateur d'intéragir avec la bibliothèque.
+    Zones pour AJOUTER un nouveau livre, afficher la LISTE des livres, afficher les DÉTAILS d'un livre sélectionné.
+
+
+    2. Assurer que les champs de saisie pour ajouter ou mettre à jour un livre sont validés avant d'ajouter ou de modifier des données.
+
+    3. événements JS pour géer les intéractions de l'utilisateur avec l'interface
+
+    4. Fonctionnalité gestion des catégories pour organiser les livres dans la bibliothèque.
+    Permettre aux utilisateurs d'ajouter, de modifier, et de supprimer des catégories, ainsi que d'attribuer des catégories à chaque livre.
+    Utiliser un fichier JSON pour stocker les données des livres.
+
+    5. fonctions JS pour charger, ajouter, répertorier, supprimer et mettre à jour les livres dans la bibliothèque. Utiliser le même fichier JSON pour stocker les données des livres.
+
+    6. fonctionnalité de recherche avancée à l'application de gestion de bibliothèque qui permet à l'utilisateur de spécifier des critère de recherche avancés: fourchette de dates de publication, le nombre de pages, et autres propriétés spécifiques.
+
+    7. Améliorer l'expérience utilisateur de l'application en ajoutant une fonction de suggestion automatique lors de la saisie dans le champ de recherche.
+
+    8. Ajouter des fonctionnalités de tri et de filtrage dynamiques. Permettre à l'utilisateur de trier la liste des livres par titre, auteur, date de publication ou tout autre critère pertinent en cliquant sur les en-têtes de colonne correspondants.
+
+    9. Implémenter une fonctionnalité de pagination pour gérer l'affichage des résultats de recherche et de la liste des livres.
+
+    10. Fournir une interface réative et s'adaptant à différentes tailles d'écran.
+-->
--- script.js
+++ script.js
@@ -3,27 +3,94 @@ async function loadJson(url){
     const data = await response.json();
     return data;
 }
+
 document.addEventListener("DOMContentLoaded", function(){
+    /* chargement du modèle depuis model.json */
+    fetch('model.json')
+    .then(response => response.json())
+    .then(data => displayData(data))
+    .catch(error => console.error('Erreur lors de la lecture du fichier JSON:', error));
+
+    const detailImage = document.getElementById('detail-image');
+    const detImg = document.createElement('img');
+    const table = document.createElement('table');
+    const infos = document.getElementById('infos');
+    
+
+    function displayData(userData){
+        const entries= Object.entries(userData);
+        const detailsDiv =  document.getElementById('details');
+
+        table.style.textWrap = "wrap";
+        infos.appendChild(table);
+        detailImage.appendChild(detImg);
+        detImg.src = userData.image;
+        
+        for(let entry of entries){
+            var row = document.createElement('tr');
+            var col1 = document.createElement('td');
+            var col2 = document.createElement('td');
+            
+            var string ="";
+            col1.classList.add('col1');
+            table.appendChild(row);
+            row.appendChild(col1);
+            row.appendChild(col2);
+
+            switch(entry[0]){
+                case "datePublication":
+                    entry[0]="Date de publication";
+                    break;
+                case "editeur":
+                    entry[0]="Éditeur";
+                    break;
+                case "resume":
+                    entry[0]="Résumé";
+                    break;
+                case "nombrePages":
+                    entry[0]="Nombre de pages";
+                    break;
+                case "disponibilite":
+                    entry[0]="Disponibilité";
+                    break;
+                case "etat":
+                    entry[0]="État";
+                    break;
+                default:
+                    string = entry[0].charAt(0).toUpperCase()+entry[0].slice(1);
+                    entry[0]=string;
+                    break;
+
+            }
+            col1.innerText = entry[0];
+            col2.innerText =  entry[1];
+        }
+    }
+
+
     const liste =  document.getElementById('liste');
     const ul =  document.createElement('ul');
     liste.appendChild(ul);
-    for(var i=1;i<=24;i++){
+    for(var i=1;i<=12;i++){
         var li = document.createElement('li');
-        var button =  document.createElement('button');
+        var intern =  document.createElement('a');
         var img = document.createElement('img');
         
-        button.innerText="Voir plus";
+        intern.innerText="Détails";
+        intern.setAttribute('href',"#details")
         li.classList.add('items');
         img.src ='img/bd1.jpg';
         img.classList.add('thumbnails');
         li.appendChild(img);
-        /*li.appendChild(button);*/
+        li.appendChild(intern);
         li.classList.add('books');
         ul.appendChild(li);
     }
+
     /*
     loadJson('model.json').then(data => console.log(data)).catch(error => console.error('Erreur lors du chargement du fichier JSON:',error));
     */
+   
     const datePublication = document.getElementById('datePublication');
     const today = new Date();
     datePublication.setAttribute('max',today.getFullYear());
